48fa9fecd45630e5cb8c5000aafe8bea73068bd6
luis leiva <lileiva@uc.cl>
2020-07-04 15:52:20 -0400

    add styled components suport

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,16 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import tw from 'twin.macro'
 
+const SButton = styled.button(() => [
+  tw`text-red-300
+  m-3
+  bg-gray-300
+  p-8
+  `,
+])
+
+const Button = () => <button type="button">Success</button>
 const defaultTitle = 'hello from next js'
 const Index = ({ title = defaultTitle }) => (
   <div>
@@ -7,6 +18,9 @@ const Index = ({ title = defaultTitle }) => (
     <p className="text-gray-900 m-3">
       with Tailwind CSS & postcss-preset-env. Now uses tailwind purge method for
       production builds.
+      <Button />
+      <SButton> texto </SButton>
+
     </p>
   </div>
 )
